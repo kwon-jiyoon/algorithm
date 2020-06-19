c4ad8aae86e1b41eae7e2c745a4abec8dc493b4a
kwon-jiyoon <kwon31403@gmail.com>
2020-06-19 16:11:53 +0900

    추가

--- 1212.py
+++
@@ -1,2 +0,0 @@
-aksdfnasfldasdfjaskdf
-asdfasdf
---
+++ four-point poeration.py
@@ -0,0 +1,9 @@
+data = input().split(" ")
+A = int(data[0])
+B = int(data[1])
+
+print(A + B)
+print(A - B)
+print(A * B)
+print(int(A / B))
+print(A % B)
